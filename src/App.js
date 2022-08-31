3537248028dc70c89a1efdbb5c5c9e87752d8406
shaktimanrai <98007119+shaktimanrai@users.noreply.github.com>
2022-08-31 11:40:31 -0700

    Add Colculet react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Add, Sub, Mult, Div} from './components/Colculater'
+
+const App = () => {
+  return (<>
+  <ul>
+<li>sum of two no is {Add(40,4)}</li>
+<li>Sub of two no is {Sub(40,4)}</li>
+<li>Mult of two no is {Mult(40,4)}</li>
+<li>Div of two no is {Div(40,4)}</li>
+
+
+  </ul>
+  
+  </>
+   
+  )
+}
+
+export default App
